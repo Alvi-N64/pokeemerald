f47ef712d1e1b79d1193954914d69f53c192f487
Alvi N <alpi2games@gmail.com>
2026-06-14 18:46:12 +0800

    Update pokedex.c

--- src/pokedex.c
+++ src/pokedex.c
@@ -4151,52 +4151,45 @@ static void PrintMonInfo(u32 num, u32 value, u32 owned, u32 newEntry)
     PrintInfoScreenText(description, GetStringCenterAlignXOffset(FONT_NORMAL, description, DISPLAY_WIDTH), 95);
 }
 
-static void PrintMonHeight(u16 height, u8 left, u8 top)
+static void PrintMonHeight(u16 height, u8 left, u8 top) // balik pake meter
 {
     u8 buffer[16];
-    u32 inches, feet;
     u8 i = 0;
-
-    inches = (height * 10000) / 254;
-    if (inches % 10 >= 5)
-        inches += 10;
-    feet = inches / 120;
-    inches = (inches - (feet * 120)) / 10;
 
     buffer[i++] = EXT_CTRL_CODE_BEGIN;
     buffer[i++] = EXT_CTRL_CODE_CLEAR_TO;
-    if (feet / 10 == 0)
+
+    if (height / 100 == 0)
     {
         buffer[i++] = 18;
-        buffer[i++] = feet + CHAR_0;
+        buffer[i++] = CHAR_SPACE;
     }
     else
     {
         buffer[i++] = 12;
-        buffer[i++] = feet / 10 + CHAR_0;
-        buffer[i++] = (feet % 10) + CHAR_0;
+        buffer[i++] = (height / 100) + CHAR_0;
     }
-    buffer[i++] = CHAR_SGL_QUOTE_RIGHT;
-    buffer[i++] = (inches / 10) + CHAR_0;
-    buffer[i++] = (inches % 10) + CHAR_0;
-    buffer[i++] = CHAR_DBL_QUOTE_RIGHT;
+
+    buffer[i++] = ((height % 100) / 10) + CHAR_0;
+    buffer[i++] = CHAR_COMMA;
+    buffer[i++] = (height % 10) + CHAR_0;
+
+    buffer[i++] = CHAR_SPACE;
+    buffer[i++] = CHAR_m;
     buffer[i++] = EOS;
+
     PrintInfoScreenText(buffer, left, top);
 }
 
-static void PrintMonWeight(u16 weight, u8 left, u8 top)
+static void PrintMonWeight(u16 weight, u8 left, u8 top) // balik pake kilogram
 {
     u8 buffer[16];
     bool8 output;
-    u8 i;
-    u32 lbs = (weight * 100000) / 4536;
+    u8 i = 0;
 
-    if (lbs % 10u >= 5)
-        lbs += 10;
-    i = 0;
     output = FALSE;
 
-    if ((buffer[i] = (lbs / 100000) + CHAR_0) == CHAR_0 && !output)
+    if ((buffer[i] = (weight / 1000) + CHAR_0) == CHAR_0 && !output)
     {
         buffer[i++] = CHAR_SPACER;
     }
@@ -4206,8 +4199,8 @@ static void PrintMonWeight(u16 weight, u8 left, u8 top)
         i++;
     }
 
-    lbs %= 100000;
-    if ((buffer[i] = (lbs / 10000) + CHAR_0) == CHAR_0 && !output)
+    weight %= 1000;
+    if ((buffer[i] = (weight / 100) + CHAR_0) == CHAR_0 && !output)
     {
         buffer[i++] = CHAR_SPACER;
     }
@@ -4217,28 +4210,16 @@ static void PrintMonWeight(u16 weight, u8 left, u8 top)
         i++;
     }
 
-    lbs %= 10000;
-    if ((buffer[i] = (lbs / 1000) + CHAR_0) == CHAR_0 && !output)
-    {
-        buffer[i++] = CHAR_SPACER;
-    }
-    else
-    {
-        output = TRUE;
-        i++;
-    }
-
-    lbs %= 1000;
-    buffer[i++] = (lbs / 100) + CHAR_0;
-    lbs %= 100;
-    buffer[i++] = CHAR_PERIOD;
-    buffer[i++] = (lbs / 10) + CHAR_0;
+    weight %= 100;
+    buffer[i++] = (weight / 10) + CHAR_0;
+    buffer[i++] = CHAR_COMMA;
+    buffer[i++] = (weight % 10) + CHAR_0;
+    
     buffer[i++] = CHAR_SPACE;
-    buffer[i++] = CHAR_l;
-    buffer[i++] = CHAR_b;
-    buffer[i++] = CHAR_s;
-    buffer[i++] = CHAR_PERIOD;
+    buffer[i++] = CHAR_k;
+    buffer[i++] = CHAR_g;
     buffer[i++] = EOS;
+
     PrintInfoScreenText(buffer, left, top);
 }
 
